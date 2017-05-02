3d123dfe1433b6df0659519b4f324712103c344b
Sarah Widder <sarah.j.widder@gmail.com>
2017-05-02 11:47:15 -0400

    fixed some compilation issues. progress on recording training data.

--- lib/gesture_recognition/include/gesture_recognition.h
+++ lib/gesture_recognition/include/gesture_recognition.h
@@ -3,23 +3,95 @@
 
 #include <ros/ros.h>
 #include <ros/console.h>
-
+#include <aruco_msgs/MarkerArray.h>
 // #include "robot_utils/utils.h"
 
 #include <GRT/GRT.h>
-// #include "robot_perception/aruco_client.h"
+// #include "../../../../baxter_collaboration/baxter_collaboration_lib/include/robot_perception/aruco_client.h"
 
 class GestureRec // : public ARucoClient
 {
 private:
+
+
+    ros::NodeHandle _nh;
+
+    std::string _limb; // Limb of the gripper: left or right
+
+    // Subscriber to the ARuco detector,
+    ros::Subscriber _aruco_sub;
+
+    // List of available markers
+    std::vector<int> available_markers;
+
+        // Bool to check if ARuco is fine or not
+    bool              aruco_ok;
+
+    // Bool to check if there are any markers detected
+    bool         markers_found;
+
+    // Bool to check if the selected marker was found or not
+    bool          marker_found;
+
+    // ID of the marker to detect
+    int              marker_id;
+    // Marker position and orientation
+    geometry_msgs::Point        curr_marker_pos;
+    geometry_msgs::Quaternion   curr_marker_ori;
+
+
+
+protected:
+    /**
+     * Callback function for the ARuco topic
+     * @param msg the topic message
+     */
+    void ARucoCb(const aruco_msgs::MarkerArray& msg);
+
+        /*
+     * Check availability of the ARuco data
+     * @return true/false if feedback from ARuco is received
+    */
+    bool isARucoOK() { return aruco_ok; };
+
+
+        /* SETTERS */
+    void setMarkerID(int _id)                { marker_id = _id; };
+
+    /* GETTERS */
+    geometry_msgs::Point      getMarkerPos() { return curr_marker_pos; };
+    geometry_msgs::Quaternion getMarkerOri() { return curr_marker_ori; };
+
+    int         getMarkerID()  { return marker_id; };
+
+    /**
+     * Returns a list of available markers
+     * @return a list of available markers
+     */
+    std::vector<int> getAvailableMarkers() { return available_markers; };
+
+    /**
+     * Looks if a set of markers is present among those available.
+     * @return the subset of available markers among those available
+     */
+    std::vector<int> getAvailableMarkers(std::vector<int> _markers);
+
+    /**
+     * @brief records 1 second of ARuco data
+     * @return true/false if success/failure
+     */
+    bool recordSample(GRT::ClassificationData trainingData, GRT::UINT gestureLabel);
+
     /**
      * @brief record & save sample data
      * @return true/false if success/failure
      */
     bool recTrainingData();
 
+
+
 public:
-    GestureRec();
+    GestureRec(std::string name, std::string limb);
     ~GestureRec();
 
 };
--- lib/gesture_recognition/src/gesture_recognition.cpp
+++ lib/gesture_recognition/src/gesture_recognition.cpp
@@ -2,45 +2,95 @@
 
 using namespace std;
 
-GestureRec::GestureRec()
+GestureRec::GestureRec(string name, string limb) : _nh(name), _limb(limb),
+                                      aruco_ok(false), markers_found(false),
+                                         marker_found(false), marker_id(-1)
 {
+    _aruco_sub = _nh.subscribe("/aruco_marker_publisher/markers",
+                               3, &GestureRec::ARucoCb, this);
+}
 
+void GestureRec::ARucoCb(const aruco_msgs::MarkerArray& msg)
+{
+    if (msg.markers.size() > 0)
+    {
+        available_markers.clear();
+    }
+
+    for (size_t i = 0; i < msg.markers.size(); ++i)
+    {
+        // ROS_DEBUG("Processing marker with id %i",msg.markers[i].id);
+
+        available_markers.push_back(int(msg.markers[i].id));
+        markers_found = true;
+
+        if (int(msg.markers[i].id) == getMarkerID())
+        {
+            curr_marker_pos = msg.markers[i].pose.pose.position;
+            curr_marker_ori = msg.markers[i].pose.pose.orientation;
+
+
+            ROS_DEBUG("Marker is in: %g %g %g", curr_marker_pos.x,
+                                                curr_marker_pos.y,
+                                                curr_marker_pos.z);
+            // ROS_INFO("Marker is in: %g %g %g %g", curr_marker_ori.x,
+            //                                       curr_marker_ori.y,
+            //                                       curr_marker_ori.z,
+            //                                       curr_marker_ori.w);
+
+            if (!marker_found)
+            {
+                marker_found = true;
+            }
+        }
+    }
+
+    if (!aruco_ok)
+    {
+        aruco_ok = true;
+    }
+}
+
+bool GestureRec::recordSample(GRT::ClassificationData trainingData, GRT::UINT gestureLabel)
+{
+    vector<vector<vector<double>>> gesture;
+    ros::Time time_start = ros::Time::now();
+
+    while( ros::Time::now().toSec() - time_start.toSec() < 1)
+    {
+        vector<vector<double>> sample(2);
+
+        vector<double> position(3);
+        position[0] = curr_marker_pos.x;
+        position[1] = curr_marker_pos.y;
+        position[2] = curr_marker_pos.z;
+
+        vector<double> orientation(4);
+        orientation[0] = curr_marker_ori.x;
+        orientation[1] = curr_marker_ori.y;
+        orientation[2] = curr_marker_ori.z;
+        orientation[3] = curr_marker_ori.w;
+
+        sample[0] = position;
+        sample[1] = orientation;
+
+        gesture.push_back(sample);
+    }
+
+    // trainingData.addSample( gestureLabel, gesture);
+
+    return true;
 }
 
 bool GestureRec::recTrainingData()
 {
     //Create a new instance of the ClassificationData
     GRT::ClassificationData trainingData;
-
-    //Set the dimensionality of the data
-    trainingData.setNumDimensions( 3 );
-
-    // For now, this will work for straight-line gestures
-    // each gesture must have a different endpoint relative to starting point
-
-    if (!waitForARucoData()) return false;
-    double start_x = getMarkerPos().x;
-    double start_y = getMarkerPos().y;
-    double start_z = getMarkerPos().z;
-
-    // gestures will be 1s long
-    ros::Duration(1.0).sleep();
-
-    if (!waitForARucoData()) return false;
-    double delta_x = getMarkerPos().x - start_x;
-    double delta_y = getMarkerPos().y - start_y;
-    double delta_z = getMarkerPos().z - start_z;
-
     GRT::UINT gestureLabel = 1;
-    vector< double > sample(3);
-    sample[0] = delta_x;
-    sample[1] = delta_y;
-    sample[2] = delta_z;
 
-    //Add the sample to the training data
-    trainingData.addSample( gestureLabel, sample );
+    if (!recordSample(trainingData, gestureLabel)) return false;
 
-    bool saveResult = trainingData.save( "TrainingData.grt" );
+    // bool saveResult = trainingData.save( "TrainingData.grt" );
 
     return true;
 
